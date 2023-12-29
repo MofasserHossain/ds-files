6f80cace887debc6bb76ccb5345b30d7fd2b617c
MofasserHossain <mofasserhossain01@gmail.com>
2023-12-29 06:03:31 +0600

    new code

--- class-work/SelectionSort.cpp
+++ class-work/SelectionSort.cpp
@@ -4,38 +4,38 @@ using namespace std;
 int selectionSort(int A[], int Size)
 {
 	int i, j, smallest;
-		for(j=1; j<Size; j++)
+	for (j = 1; j < Size; j++)
+	{
+		smallest = j;
+		for (i = j + 1; i <= Size; i++)
 		{
-			smallest = j;
-		for(i=j+1; i<=Size; i++)
-		{
-			if(A[i]<A[smallest])
+			if (A[i] < A[smallest])
 				smallest = i;
 		}
 		swap(A[j], A[smallest]);
-		}
-	return 0;	
 	}
-	
+	return 0;
+}
+
 int main()
 {
 	int Size, i;
-	cout<<"Enter the array Size: ";
-	cin>>Size;
+	cout << "Enter the array Size: ";
+	cin >> Size;
 	int A[Size];
 	srand(time(0));
-	for(i=1; i<=Size; i++)
-		A[i] = rand()%50;
-	
-	cout<<"Array elements are: ";
-	for(i=1; i<=Size; i++)
-		cout<<A[i]<<" ";
-	
-	selectionSort(A, Size);
-	
-	cout<<"\nArray elements after sorting: ";
-	for(i=1; i<=Size; i++)
-		cout<<A[i]<<" ";
+	for (i = 1; i <= Size; i++)
+		A[i] = rand() % 50;
 
-	cout<<endl;
-	}
+	cout << "Array elements are: ";
+	for (i = 1; i <= Size; i++)
+		cout << A[i] << " ";
+
+	selectionSort(A, Size);
+
+	cout << "\nArray elements after sorting: ";
+	for (i = 1; i <= Size; i++)
+		cout << A[i] << " ";
+
+	cout << endl;
+}
--- practice/test.cpp
+++ practice/test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 
-    int i, position = -1, value, size;
+    int i, position = -1, value, size, start, end, mid;
 
     cout << "Enter the size of the array : \t";
 
@@ -28,22 +28,35 @@ int main()
     cout << "\nEnter the value you want to search: ";
     cin >> value;
 
-    for (i = 1; i <= size; i++)
+    start = 1;
+    end = size;
+    mid = (start + end) / 2;
+
+    while (start <= end)
     {
-        if (array[i] == value)
+        if (array[mid] == value)
         {
-            position = i;
+            position = mid;
             break;
         }
+        else if (array[mid] < value)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
     }
 
     if (position == -1)
     {
-        cout << "Data not found";
+        cout << "Value not found";
     }
     else
     {
-        cout << position << " is the location of search item " << value;
+        cout << "Value found at position " << position;
     }
 
     cout << endl;
